856b9d80d46bdc6a10d1897b22256de72e1300ab
Wanijya <wanijyabisen@gmail.com>
2023-11-06 03:23:38 +0530

    flash message

--- endgm/routes/users.js
+++ endgm/routes/users.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require("mongoose");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+mongoose.connect("mongodb://127.0.0.1:21017/testendgm2");
+
+mongoose.Schema({
+  username: String,
+  password: String,
 });
-
-module.exports = router;
