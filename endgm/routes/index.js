6c15699a60d9a15111294668d47502ac31bc9e75
Wanijya <wanijyabisen@gmail.com>
2023-11-06 03:05:23 +0530

    flash messages

---
+++ endgm/routes/index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res) {
+  res.render("index");
+});
+
+router.get("/failed", function (req, res) {
+  req.flash("age", 11);
+  req.flash("name", "wani");
+  res.send("bangya");
+});
+
+router.get("/checkkro", function (req, res) {
+  console.log(req.flash("age"), req.flash("name"));
+  res.send("check kro backend ke terminal pe!");
+});
+
+module.exports = router;
