66b431cec229ebdb5320b78a924627a0c02ae3b7
Wanijya <wanijyabisen@gmail.com>
2023-11-05 22:55:53 +0530

    Cookies setup,read,delete

--- lolopopo/routes/index.js
+++ lolopopo/routes/index.js
@@ -1,51 +1,19 @@
 var express = require("express");
 var router = express.Router();
-const userModel = require("./users");
 
 /* GET home page. */
 router.get("/", function (req, res) {
-  req.session.ban = true;
+  res.cookie("age", 25);
   res.render("index");
 });
-router.get("/checkban", function (req, res) {
-  if (req.session.ban === true) {
-    res.send("you are baned!");
-  } else {
-    res.send("not baned!");
-  }
-});
-router.get("/removeban", function (req, res) {
-  req.session.destroy(function (err) {
-    console.log(err);
-    res.send("ban removed!");
-  });
-});
 
-router.get("/create", async function (req, res) {
-  const createduser = await userModel.create({
-    username: "wanijyaly",
-    age: 20,
-    name: "Wanijya",
-  });
-  res.send(createduser);
+router.get("/read", function (req, res) {
+  console.log(req.cookies.age);
+  res.send("check!")
 });
-
-router.get("/allusers", async function (req, res) {
-  const allusers = await userModel.find();
-  res.send(allusers);
-});
-
-router.get("/alluser", async function (req, res) {
-  const alluser = await userModel.findOne({ username: "aarya" });
-  console.log(alluser);
-  res.send(alluser);
-});
-
-router.get("/delete", async function (req, res) {
-  const deleteduser = await userModel.findOneAndDelete({
-    username: "wanijyaly",
-  });
-  res.send(deleteduser);
+router.get("/delete", function (req, res) {
+  res.clearCookie("age");
+  res.send("done!")
 });
 
 module.exports = router;
