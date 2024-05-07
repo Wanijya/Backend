1bd43f135ce9cbe66e4fad905c4a8913e8a330a0
Wanijya <wanijyabisen@gmail.com>
2024-05-08 02:53:03 +0530

    noteTakingWebApp for Practice

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
+const fs = require('fs');
+
+app.set('view engine', 'ejs');
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'public'))); //express se kahe rhe hai ki static files es wale path par search krna public wale path par, our wo chala gya app.use() ke andar.
+app.use(express.urlencoded({ extended:true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('view engine', 'ejs'); //aapka jo backend hai wo kya render krega, wo ejs pages show krega.
+app.get("/", function(req, res){
+    fs.readdir(`./files`, function(err, files){ 
+        res.render("index", {files: files});
+    });
+});
 
-app.get("/", function(req, res) {
-    res.render("index");
-})
+app.get("/file/:filename", function(req, res){
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata){
+        res.render("show", {filename: req.params.filename, filedata: filedata});
+    });
+});
 
-app.get("/profile/:username", function(req, res){
-    res.send(`Welcome, ${req.params.username}`);
-})
+app.get("/edit/:filename", function(req, res){
+    res.render('edit', {filename: req.params.filename});
+});
 
-app.get("/author/:username/:age", function(req, res){
-    res.send(`Welcome, ${req.params.username} of age ${req.params.age}`);
-})
+app.post("/edit", function(req, res){
+   fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, function(err){
+    res.redirect("/");
+   })
+});
 
-app.listen(3000, function(){
-    console.log("Its Running!!")
-})
+app.post("/create", function(req, res){
+   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+    res.redirect("/");
+   });
+});
+
+app.listen(3000);
