e22fe67224b971767f7e6c4c052ba6df2b6e500c
Wanijya <wanijyabisen@gmail.com>
2024-05-21 02:20:55 +0530

    How to use password encrytption and decryption

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const cookieParser = require('cookie-parser');
 const express = require('express');
-const bycrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const app = express();
 
 
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
-    res.cookie("name", "Aaru");
-    res.send("done");
-})
-
-app.get('/read', (req, res) => {
-    console.log(req.cookies);
-    res.send("read page");
+    // bcrypt.genSalt(10, function(err, salt) {
+    //     bcrypt.hash("pololololoo", salt, function(err, hash) {
+    //         console.log(hash);
+    //     });
+    // });
+    bcrypt.compare("pololololoo", "$2b$10$iP7qiQXbROizAwpswiGP7e.8CxAWtMzdYxIKMeUOjNGcMfPYKsenO", function(err, result) {
+        console.log(result)
+    });
 })
 
 app.listen(3000)
