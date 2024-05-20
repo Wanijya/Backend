732b496a50888d9626d57687d2f51faab086a9e5
Wanijya <wanijyabisen@gmail.com>
2024-05-21 03:02:52 +0530

    all about jwt

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const app = express();
-
 
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    // bcrypt.genSalt(10, function(err, salt) {
-    //     bcrypt.hash("pololololoo", salt, function(err, hash) {
-    //         console.log(hash);
-    //     });
-    // });
-    bcrypt.compare("pololololoo", "$2b$10$iP7qiQXbROizAwpswiGP7e.8CxAWtMzdYxIKMeUOjNGcMfPYKsenO", function(err, result) {
-        console.log(result)
-    });
-})
+app.get("/", (req, res) => {
+  let token = jwt.sign({ email: "wani@example.com" }, "secret");
+  res.cookie("token", token);
+  res.send("Done!");
+});
+app.get("/read", (req, res) => {
+  let data = jwt.verify(req.cookies.token, "secret");
+  console.log(data);
+});
 
-app.listen(3000)
+app.listen(3000);
