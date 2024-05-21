3b66ec359b5ee42383eb4fb201698ad258f2f3c5
Wanijya <wanijyabisen@gmail.com>
2024-05-21 18:09:58 +0530

    Authentication & Authorization | JWT Token | MongoDB

--- app.js
+++ app.js
@@ -1,19 +1,62 @@
 const express = require("express");
+const app = express();
+const userModel = require("./models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const cookieParser = require("cookie-parser");
-const app = express();
 
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const { hash } = require("crypto");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  let token = jwt.sign({ email: "wani@example.com" }, "secret");
-  res.cookie("token", token);
-  res.send("Done!");
+  res.render("index");
 });
-app.get("/read", (req, res) => {
-  let data = jwt.verify(req.cookies.token, "secret");
-  console.log(data);
+
+app.post("/create", (req, res) => {
+  let { username, email, password, age } = req.body;
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      let createdUser = await userModel.create({
+        username,
+        email,
+        password: hash,
+        age,
+      });
+
+      let token = jwt.sign({ email }, "shhhhhhhhhhh");
+      res.cookie("token", token);
+      res.send(createdUser);
+    });
+  });
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.post("/login", async function (req, res) {
+  let user = await userModel.findOne({ email: req.body.email });
+  if (!user) return res.send("something went wrong!!");
+
+  bcrypt.compare(req.body.password, user.password, function (err, result) {
+    if (result) {
+      let token = jwt.sign({ email: user.email }, "shhhhhhhhhhh");
+      res.cookie("token", token);
+      res.send("you can now login!");
+    } else res.send("something went wrong!!");
+  });
+});
+
+app.get("/logout", (req, res) => {
+  res.cookie("token", "");
+  res.redirect("/");
 });
 
 app.listen(3000);
