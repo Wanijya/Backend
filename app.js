ebfd63cb5ed5fa2e4ab2efa32b8c8b12fc13c30c
Wanijya <wanijyabisen@gmail.com>
2024-05-21 18:46:29 +0530

    Data Association | MongoDB

--- app.js
+++ app.js
@@ -1,62 +1,32 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const userModel = require("./models/user");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const userModel = require('./models/user');
+const postModel = require('./models/posts');
 
-const cookieParser = require("cookie-parser");
-const path = require("path");
-const { hash } = require("crypto");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
 
-app.set("view engine", "ejs");
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(cookieParser());
-
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/create", async (req, res) => {
+  let user = await userModel.create({
+    username: "Wani",
+    email: "Wani@example.com",
+    age: 21,
+  })
+  res.send(user);
 });
 
-app.post("/create", (req, res) => {
-  let { username, email, password, age } = req.body;
+app.get("/post/create", async (req, res) => {
+  let post = await postModel.create({
+    postdata: "saare log kese hoo..",
+    user: "664c9d771c43ec0f8e667c08",
+  })
 
-  bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash(password, salt, async (err, hash) => {
-      let createdUser = await userModel.create({
-        username,
-        email,
-        password: hash,
-        age,
-      });
+  let user = await userModel.findOne({_id: "664c9d771c43ec0f8e667c08"});
+  user.posts.push(post._id);
+  await user.save();
 
-      let token = jwt.sign({ email }, "shhhhhhhhhhh");
-      res.cookie("token", token);
-      res.send(createdUser);
-    });
-  });
-});
-
-app.get("/login", function (req, res) {
-  res.render("login");
-});
-
-app.post("/login", async function (req, res) {
-  let user = await userModel.findOne({ email: req.body.email });
-  if (!user) return res.send("something went wrong!!");
-
-  bcrypt.compare(req.body.password, user.password, function (err, result) {
-    if (result) {
-      let token = jwt.sign({ email: user.email }, "shhhhhhhhhhh");
-      res.cookie("token", token);
-      res.send("you can now login!");
-    } else res.send("something went wrong!!");
-  });
-});
-
-app.get("/logout", (req, res) => {
-  res.cookie("token", "");
-  res.redirect("/");
+  res.send({post, user});
 });
 
 app.listen(3000);
--- models/user.js
+++ models/user.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://127.0.0.1:27017/authtestapp`);
+mongoose.connect("mongodb://127.0.0.1:27017/testingdb");
 
 const userSchema = mongoose.Schema({
     username: String,
     email: String,
-    password: String,
     age: Number,
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "posts"
+        }
+    ],
 });
 
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model("user", userSchema);
