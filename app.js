38f177b38444ff43bdf16f5cba125c827e089d8f
Wanijya <wanijyabisen@gmail.com>
2024-05-29 18:02:59 +0530

    Multer

--- app.js
+++ app.js
@@ -5,14 +5,28 @@ const postModel = require("./models/post");
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const upload = require("./config/multerconfig");
+const path = require("path");
 
 app.set("view engine", "ejs");
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.get("/", function(req, res){
     res.render("index");
+});
+
+app.get("/profile/upload", function(req, res){
+    res.render("profileupload");
+});
+
+app.post("/upload", isLoggedIn, upload.single("image"), async function(req, res){
+    let user = await userModel.findOne({email: req.user.email});
+    user.profilepic = req.file.filename;
+    await user.save();
+    res.redirect("/profile");
 });
 
 app.get("/profile", isLoggedIn, async function(req, res){
