7d18b125cb03d6353b73bc0dceec871c85163f73
Wanijya <wanijyabisen@gmail.com>
2024-05-29 16:45:11 +0530

    Mini Project Data Association

--- app.js
+++ app.js
@@ -1,32 +1,113 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const userModel = require('./models/user');
-const postModel = require('./models/posts');
+const userModel = require("./models/user");
+const postModel = require("./models/post");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.get("/create", async (req, res) => {
-  let user = await userModel.create({
-    username: "Wani",
-    email: "Wani@example.com",
-    age: 21,
-  })
-  res.send(user);
+app.get("/", function(req, res){
+    res.render("index");
 });
 
-app.get("/post/create", async (req, res) => {
-  let post = await postModel.create({
-    postdata: "saare log kese hoo..",
-    user: "664c9d771c43ec0f8e667c08",
-  })
-
-  let user = await userModel.findOne({_id: "664c9d771c43ec0f8e667c08"});
-  user.posts.push(post._id);
-  await user.save();
-
-  res.send({post, user});
+app.get("/profile", isLoggedIn, async function(req, res){
+    let user = await userModel.findOne({email: req.user.email}).populate("posts")
+    res.render("profile", {user});
 });
+app.get("/like/:id", isLoggedIn, async function(req, res){
+    let post = await postModel.findOne({_id: req.params.id}).populate("user");
+    
+    if(post.likes.indexOf(req.user.userid) === -1){
+        post.likes.push(req.user.userid);
+    }
+    else{
+        post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+    }
+    await post.save();
+    res.redirect("/profile");
+});
+
+app.get("/edit/:id", isLoggedIn, async function(req, res){
+    let post = await postModel.findOne({_id: req.params.id}).populate("user");
+    res.render("edit", {post});
+});
+
+app.post("/update/:id", isLoggedIn, async function(req, res){
+    let post = await postModel.findOneAndUpdate({_id: req.params.id}, {content: req.body.content})
+    res.redirect("/profile");
+});
+
+app.post("/post", isLoggedIn, async function(req, res){
+    let user = await userModel.findOne({email: req.user.email});
+    let {content} = req.body;
+    let post = await postModel.create({
+        user: user._id,
+        content
+    });
+    user.posts.push(post._id);
+    await user.save();
+    res.redirect("/profile");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.post("/register", async function(req, res){
+    let {email, password, username, name, age} = req.body;
+
+    let user =  await userModel.findOne({email});
+    if(user) return res.status(500).send("User already registered");
+
+    bcrypt.genSalt(10,(err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) => {
+            let user = await userModel.create({
+                username,
+                name,
+                age,
+                email,
+                password:  hash,
+            })
+            let token = jwt.sign({email: email, userid: user._id}, "shhh");
+            res.cookie("token", token);
+            res.send("Registred");
+        });
+    });
+});
+
+app.post("/login", async function(req, res){
+    let {email, password} = req.body;
+
+    let user =  await userModel.findOne({email});
+    if(!user) return res.status(500).send("Something Went wrong");
+
+    bcrypt.compare(password, user.password, async (err, result) => {
+        if(result) {
+            let token = jwt.sign({email: email, userid: user._id}, "shhh");
+            res.cookie("token", token);
+            res.status(200).redirect("/profile");
+        }
+        else res.redirect("/login");
+    })
+});
+
+app.get("/logout", function(req, res){
+    res.cookie("token", "");
+    res.redirect("/login");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.cookies.token === "") res.redirect("/login");
+    else {
+        let data = jwt.verify(req.cookies.token, "shhh");
+        req.user = data;
+        next();
+    }
+};
 
 app.listen(3000);
--- models/user.js
+++ models/user.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/testingdb");
+mongoose.connect("mongodb://127.0.0.1:27017/miniproject");
 
-const userSchema = mongoose.Schema({
+const userSchema =  mongoose.Schema({
     username: String,
-    email: String,
+    name: String,
     age: Number,
+    email: String,
+    password: String,
     posts: [
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref: "posts"
+            ref: "post"
         }
-    ],
-});
+    ]
+})
 
 module.exports = mongoose.model("user", userSchema);
