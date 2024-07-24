3cba934e6d1e987e422f8a86df5769cb27764018
Wanijya <wanijyabisen@gmail.com>
2024-07-25 03:27:27 +0530

    cloudinary, multer, middlewares setup

--- src/index.js
+++ src/index.js
@@ -5,19 +5,18 @@ import connectDB from "./db/index.js";
 dotenv.config({ path: "./env" });
 
 connectDB()
-.then(()=>{
-  app.on("error", (error) => {
-    console.log("Error: ", error);
-    throw error;
-  });
-  
-  app.listen(process.env.PORT || 8000, ()=>{
-    console.log(`Server started on port ${process.env.PORT}`)
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server started on port ${process.env.PORT}`);
+    });
   })
-})
-.catch((err)=>{
-  console.log("Mongo DB connection Failed !! ", err)
-})
+  .catch((err) => {
+    console.log("Mongo DB connection Failed !! ", err);
+  });
 
 /*
 import express from "express";
