54b725231da69ded9823888a031650a052e5d35d
Wanijya <wanijyabisen@gmail.com>
2024-07-29 13:25:06 +0530

    all setup configured and make first route just an example.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // require('dotenv').config({path: './env'});
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 dotenv.config({ path: "./env" });
 
 connectDB()
@@ -11,7 +11,7 @@ connectDB()
       throw error;
     });
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server started on port ${process.env.PORT}`);
+      console.log(`Server started on port: ${process.env.PORT}`);
     });
   })
   .catch((err) => {
